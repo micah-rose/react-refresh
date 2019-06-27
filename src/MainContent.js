08afecb97ab38b6164bb08c4975a022bb5ba3f09
micah-rose <micah.r.lund@gmail.com>
2019-06-27 11:09:51 -0600

    course follow along code for React refresher

---
+++ src/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainContent() {
+    return (
+      <div>
+    <ul>
+    <li>Boxing</li>
+    <li>Music</li>
+    <li>Camping</li>
+    </ul>
+      </div>
+    );
+  }
+
+  export default MainContent;
