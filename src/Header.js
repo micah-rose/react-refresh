5de5662b54b2c021534c6e66af7878a71b7ca7cb
micah-rose <micah.r.lund@gmail.com>
2019-06-28 15:04:16 -0600

    added more practice funcitonality with components and styling

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header>
-        <h1>Hobbies/Interests:</h1>
+        <h1 style={{backgroundColor: "red"}}>Hobbies/Interests:</h1>
         </header>
     );
   }
--- src/MainContent.js
+++ src/MainContent.js
@@ -1,6 +1,28 @@
 import React from "react";
 
 function MainContent() {
+
+  const date = new Date();
+  const hours = date.getHours();
+  var timeOfDay;
+
+  const styles = {
+    fontSize: "40px"
+  }
+
+  if (hours < 12){
+    timeOfDay = "morning"
+    styles.color = "orange"
+  }
+  else if (hours >= 12 && hours < 17){
+    timeOfDay = "afternoon"
+    styles.color = "orange"
+  } else {
+    timeOfDay = "night"
+    styles.color = "orange"
+  }
+
+
     return (
       <div>
     <ul>
@@ -8,6 +30,8 @@ function MainContent() {
     <li>Music</li>
     <li>Camping</li>
     </ul>
+
+    <h1 style={styles}>Good {timeOfDay}!</h1>
       </div>
     );
   }
